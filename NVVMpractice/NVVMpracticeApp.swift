3c51a09f888df00496d05706f6ff974a4f88fd2c
yongjunhan <hanyongjun.dev@gmail.com>
2022-12-09 12:39:15 +0900

    Initial Commit

---
+++ NVVMpractice/NVVMpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NVVMpracticeApp.swift
+//  NVVMpractice
+//
+//  Created by YongJun Han on 2022/12/09.
+//
+
+import SwiftUI
+
+@main
+struct NVVMpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
